779cb0a62772da1865c47a49a4f58994be835564
Sarah.Xu <sarah.xu@thizgroup.com>
2019-12-26 10:37:44 +0800

    [test]

---
+++ motse_rule-dao/src/main/java/ink/moshuier/motse/dao/QuestionnairDao.java
@@ -0,0 +1,14 @@
+package ink.moshuier.motse.dao;
+
+import ink.moshuier.motse.model.entity.QuestionnairEntity;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author : Sarah Xu
+ * @date : 2019-05-28
+ */
+@Repository
+public interface QuestionnairDao
+        extends BaseDao<QuestionnairEntity> {
+
+}
---
+++ motse_rule-dao/src/main/java/ink/moshuier/motse/dao/TaskDao.java
@@ -0,0 +1,14 @@
+package ink.moshuier.motse.dao;
+
+import ink.moshuier.motse.model.entity.TaskEntity;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author : Sarah Xu
+ * @date : 2019-05-28
+ */
+@Repository
+public interface TaskDao
+        extends BaseDao<TaskEntity> {
+
+}
